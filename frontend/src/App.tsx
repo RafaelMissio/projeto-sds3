53d9687efefa754869cd70cdb9b9cf5706c7d854
Rafael Missio <rtmissio@gmail.com>
2021-05-07 21:50:18 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,19 @@
-
+import Footer from "component/Footer";
+import NavBar from "component/NavBar";
+import DataTable from "component/DataTable";
 
 function App() {
   return (
     <>
      
+     <NavBar />
       <div className="container" >
         <h1 className="text-primary py-3">Ola Mundo!</h1>
 
-        
+        <DataTable />
       </div>
       
-     
+     <Footer />
     </>
   );
 }
---
+++ frontend/src/component/Footer/index.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer mt-auto py-3 bg-dark">
+  <div className="container">
+    <p className="text-light">App desenvolvido por <a href="https://github.com/RafaelMissio" target="_blank" rel="noreferrer">Rafael Missio</a></p>
+    <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/rafaeltiagomissio/" target="_blank" rel="noreferrer">@rafaeltiagomissio</a></small></p>
+  </div>
+</footer>
+
+    </>
+  );
+}
+
+export default Footer;
